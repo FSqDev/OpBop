5dcba15a31f2739ecb9949063833e3fbe03b2271
Richard Qin <rqin1@ualberta.ca>
2021-08-21 15:06:09 -0600

    initialize blacklist

--- FrontEnd/background.js
+++ FrontEnd/background.js
@@ -1,5 +1,10 @@
 // background.js
 
 chrome.runtime.onInstalled.addListener(() => {
+  chrome.storage.sync.get("blackList", (data) => {
+    if (data.blackList == null) {
+      chrome.storage.sync.set({ blackList: [] });
+    }
+  })
   console.log('started');
 });
