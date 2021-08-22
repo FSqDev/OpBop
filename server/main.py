2a6f336e8087d1cbf76421c312ba0d5630a4045f
bgreni <grenierb96@gmail.com>
2021-08-21 22:30:56 -0600

    forgot to add tsv file oopsies

--- server/main.py
+++ server/main.py
@@ -22,15 +22,6 @@ news_utils = NewsUtils()
 cors = CORS(app)
 
 reliability_data = load_reliability_data()
-pprint(reliability_data)
-
-
-# @app.after_request
-# def after_request(response):
-#     response.headers.add('Access-Control-Allow-Origin', '*')
-#     response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
-#     response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
-#     return response
 
 @app.route('/')
 def home():
